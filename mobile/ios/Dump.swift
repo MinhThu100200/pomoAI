82216cde411cfa5ba6d11552223c965ba15d2eb3
MThu <18128062@student.hcmute.edu.vn>
2025-06-20 07:25:55 +0700

    fix: firebaseAuth not found ios

---
+++ mobile/ios/Dump.swift
@@ -0,0 +1,14 @@
+//
+//  Dump.swift
+//  pomoAI
+//
+//  Created by minh thu  on 6/19/25.
+//
+
+import Foundation
+
+@objc class Dummy: NSObject {
+    @objc func dummyMethod() {
+        print("Dummy method called")
+    }
+}
---
+++ mobile/ios/pomoAI-Bringing-Header.h
@@ -0,0 +1,12 @@
+//
+//  pomoAI-Bringing-Header.h
+//  pomoAI
+//
+//  Created by minh thu  on 6/19/25.
+//
+
+#ifndef pomoAI_Bringing_Header_h
+#define pomoAI_Bringing_Header_h
+
+
+#endif /* pomoAI_Bringing_Header_h */
