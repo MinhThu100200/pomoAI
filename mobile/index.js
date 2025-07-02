fff3c99f657d833210732fca261d857f11a4c664
MThu <18128062@student.hcmute.edu.vn>
2025-07-02 07:30:36 +0700

    feat: radio button

--- mobile/index.js
+++ mobile/index.js
@@ -2,9 +2,11 @@
  * @format
  */
 globalThis.RNFB_SILENCE_MODULAR_DEPRECATION_WARNINGS = true;
-
-import {AppRegistry} from 'react-native';
+import { Buffer } from 'buffer';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
+
+global.Buffer = Buffer; //use react native svg android "Unable to resolve module buffer"
 
 AppRegistry.registerComponent(appName, () => App);
